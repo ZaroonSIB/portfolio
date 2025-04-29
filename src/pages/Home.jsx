2e72cee882e433afbbb384fcb0e4e62c0eeb7792
Zaroon <sib.zaroon@gmail.com>
2025-04-29 17:34:32 +0530

    home change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import Layout from '../layout/Layout'
-import bannerVideo from '../assets/images/artistic-seo-video.mp4';
+import Herosection from '../components/Herosection'
+
 
 const Home = () => {
   return (
     <Layout>
-        <section>
-            <video className="w-full h-{100vh}" autoPlay muted loop>
-                <source src={bannerVideo} type="video/mp4" />
-            </video>     
-        </section>
+        <Herosection/>
     </Layout>
   )
 }
